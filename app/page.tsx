4057659c29edfb87bb178bc3b05248c63bed5415
imaxisXD <sunny735084@gmail.com>
2023-10-13 02:40:38 +0530

    fixed: error

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import FileUpload from "@/components/fileUpload";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FileUpload />
+      {/* <FileUpload /> */}
     </main >
   )
 }
